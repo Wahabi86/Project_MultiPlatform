9ffdd51b2d512cbf8e71eae39f28ef662b403b6f
DegusPutra <igedeagusperdana03@gmail.com>
2025-06-16 21:41:11 +0800

    Update data dummy

--- lib/datas/data_dummy.dart
+++ lib/datas/data_dummy.dart
@@ -5,6 +5,8 @@ List<Map<String, dynamic>> banners = [
     "genre": "Action, Drama",
     "duration": "-",
     "status": "Cooming Soon",
+    "synopsis":
+        "Sancaka, seorang anak yatim yang tumbuh di jalanan setelah ditinggal kedua orang tuanya. Hidup keras membentuknya menjadi pribadi yang tertutup dan tak peduli pada keadaan sekitar. Namun, ketika kekacauan dan ketidakadilan merajalela di kotanya, Sancaka tersambar petir dan mendapatkan kekuatan luar biasa yang membuatnya mampu mengendalikan listrik. Dengan identitas baru sebagai Gundala, ia bangkit menjadi pahlawan yang melawan kejahatan, terutama tokoh jahat bernama Pengkor yang berusaha mengacaukan negeri dengan rencana beracun. Perjalanan Sancaka bukan hanya tentang kekuatan, tapi juga perjuangan moral untuk peduli dan melindungi sesama.",
     "actors": [
       {"name": "Abimana Arya", "photo": "assets/images/actors/abimana.jpg"},
       {"name": "Ario Bayu", "photo": "assets/images/actors/ario.jpg"},
@@ -18,6 +20,8 @@ List<Map<String, dynamic>> banners = [
     "genre": "Horor",
     "duration": "-",
     "status": "Cooming Soon",
+    "synopsis":
+        "mengisahkan tentang seorang polisi bernama Arya yang mengalami trauma berat setelah gagal menyelamatkan keluarganya dari pembunuhan brutal. Bertahun-tahun kemudian, ia kembali ke kampung halamannya untuk menyelidiki serangkaian pembunuhan misterius yang terjadi setiap malam bulan purnama. Seiring penyelidikan berlangsung, Arya mulai dihantui oleh mimpi-mimpi aneh dan kehadiran sosok gaib yang menyeretnya pada masa lalu kelam. Terjebak antara dunia nyata dan supranatural, Arya harus mengungkap siapa sosok pencabut nyawa sebenarnya sebelum menjadi korban berikutnya. Film ini menyajikan ketegangan psikologis dengan balutan horor misteri yang mencekam.",
     "actors": [
       {"name": "Devano Danendra", "photo": "assets/images/actors/ddevano.jpg"},
       {"name": "Keisya Levronka", "photo": "assets/images/actors/keisya.jpg"},
@@ -31,6 +35,8 @@ List<Map<String, dynamic>> banners = [
     "genre": "Action, Drama",
     "duration": "-",
     "status": "Cooming Soon",
+    "synopsis":
+        "tentang sekelompok anak muda dengan keahlian unik masing-masing yang direkrut untuk melakukan pencurian terbesar di Indonesia: mencuri lukisan legendaris “Penangkapan Pangeran Diponegoro” karya Raden Saleh dari Istana Negara. Dipimpin oleh Piko, seorang pemalsu lukisan, tim ini berisi hacker, pembalap, ahli mekanik, hingga petarung jalanan yang menjalankan misi berbahaya dengan strategi rumit demi membebaskan ayah Piko dari tuduhan palsu. Namun, di tengah misi yang penuh ketegangan dan aksi mendebarkan, mereka menyadari bahwa mereka telah dimanfaatkan untuk kepentingan politik yang lebih besar. Film ini menggabungkan aksi, ketegangan, dan drama dalam balutan kisah pencurian yang penuh kejutan.",
     "actors": [
       {"name": "Iqbaal Ramadhan", "photo": "assets/images/actors/iqbaal.jpg"},
       {"name": "Angga Yunanda", "photo": "assets/images/actors/angga.jpg"},
@@ -48,6 +54,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Horor",
     "duration": "1hr 25 min",
     "status": "XXI, Cinepolis",
+    "synopsis":
+        "mengangkat kisah kelam di sebuah desa pada tahun 1948, ketika pasangan suami istri, Sulastri dan Soedjiman, menjalani ritual terlarang demi memiliki keturunan. Mereka dikaruniai bayi kembar, Kumala dan Sumala, namun karena Sumala lahir dengan kondisi fisik yang tidak sempurna, ia dibunuh oleh ayahnya. Tindakan keji ini membangkitkan kutukan: arwah Sumala bangkit dari kematian dan menebar teror sebagai pembalasan atas penderitaan dirinya dan Kumala yang terus-menerus disiksa warga. Sosok Sumala kemudian menjadi simbol balas dendam dan kutukan mengerikan yang menghantui desa, menguak sisi gelap manusia yang diliputi keserakahan dan kekejaman.",
     "actors": [
       {"name": "Achmad Megantara", "photo": "assets/images/actors/achmad.jpg"},
       {"name": "Luna Maya", "photo": "assets/images/actors/luna.jpg"},
@@ -62,6 +70,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Romance",
     "duration": "1hr 47 min",
     "status": "XXI, Cinepolis",
+    "synopsis":
+        "perjuangan sepasang suami istri, Arya dan Mirah, yang dilanda duka mendalam setelah kehilangan anak mereka, Komang. Terpuruk dalam kesedihan, Arya dan Mirah memutuskan untuk membuat sebuah patung kayu menyerupai Komang sebagai bentuk pelampiasan rindu. Namun, kehadiran patung tersebut justru membawa perubahan aneh dalam kehidupan mereka—Mirah mulai memperlakukan patung itu seperti anak kandungnya sendiri, dan Arya pun mulai merasa kehadiran Komang kembali nyata. Dalam suasana magis yang mencekam, film ini menggambarkan konflik batin antara cinta, kehilangan, dan keikhlasan, serta bagaimana sebuah keputusasaan dapat mendorong manusia melintasi batas kenyataan dan mistis.",
     "actors": [
       {"name": "Aurora Ribero", "photo": "assets/images/actors/aurora.jpg"},
       {"name": "Kiesha Alvaro ", "photo": "assets/images/actors/alvaro.jpg"},
@@ -76,6 +86,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Comedy, Romance",
     "duration": "1hr min",
     "status": "XXI, Cinepolis",
+    "synopsis":
+        "perjalanan terakhir band lokal asal Malang bernama Yowis Ben yang dipimpin oleh Bayu. Di tengah kesuksesan dan popularitas yang mereka raih, para anggota band mulai dihadapkan pada konflik pribadi, impian yang berbeda, dan tekanan dari kehidupan dewasa. Bayu harus memilih antara karier musik atau mengejar cita-cita serta cinta sejatinya. Persahabatan, cinta, dan identitas menjadi tema utama dalam film ini, yang dikemas dengan humor khas Jawa Timur dan lagu-lagu original penuh makna. Film ini menyuguhkan akhir yang emosional dan mengharukan bagi para penggemar setia, sekaligus menjadi refleksi tentang arti kebersamaan dan keberanian melepaskan.",
     "actors": [
       {"name": "Bayu Skak", "photo": "assets/images/actors/skak.jpg"},
       {"name": "Joshua Suherman", "photo": "assets/images/actors/josua.jpg"},
@@ -90,6 +102,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Drama, Action",
     "duration": "1hr 2 min",
     "status": "XXI, Cinepolis",
+    "synopsis":
+        "menggambarkan kekosongan emosional generasi muda yang terjebak dalam dunia yang keras dan tak peduli. Cerita berpusat pada sekelompok pemuda yang tumbuh di lingkungan penuh kekerasan, kehilangan arah, dan terjebak dalam lingkaran dendam, pengkhianatan, serta pencarian jati diri. Mereka mencoba melawan keadaan dengan cara masing-masing—entah melalui pelampiasan, pemberontakan, atau pelarian—namun justru semakin jauh dari rasa kemanusiaan yang sejati. Dengan latar yang suram dan penuh tekanan, film ini menjadi cerminan tentang bagaimana kehilangan empati dan trauma masa lalu bisa mengubah seseorang menjadi dingin dan tak lagi mampu merasakan cinta atau peduli.",
     "actors": [
       {"name": "Iqbaal Ramadhan", "photo": "assets/images/actors/iqbaal.jpg"},
       {"name": "Umay Shahab", "photo": "assets/images/actors/umay.jpg"},
@@ -104,6 +118,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Comedy",
     "duration": "1hr 22min",
     "status": "XXI,Cinepolis",
+    "synopsis":
+        "petualangan kocak ketiga sahabat yang selalu terlibat dalam berbagai misi nyeleneh—mulai dari membantu masyarakat, membongkar kejahatan, hingga menghadapi situasi absurd dengan cara yang konyol dan penuh kejutan. Dengan sentuhan visual animasi modern namun tetap mempertahankan ciri khas lawakan klasik Warkop, film ini ditujukan untuk menghibur penonton lintas generasi, mengenang kejayaan Warkop DKI sekaligus memperkenalkannya kepada generasi muda dengan format yang segar dan penuh warna.",
     "actors": [
       {"name": "Indro Warkop", "photo": "assets/images/actors/indro.jpg"},
       {"name": "Aliando Syarief", "photo": "assets/images/actors/aliando.jpg"},
@@ -118,6 +134,8 @@ List<Map<String, dynamic>> movies = [
     "genre": "Comedy, Drama",
     "duration": "1hr 37min",
     "status": "XXI,NETFLIX",
+    "synopsis":
+        "lima sahabat yang memutuskan untuk membuat konten uji nyali demi meraih popularitas di media sosial. Mereka memilih lokasi sebuah rumah tua yang dikenal angker sebagai tempat pengambilan gambar. Awalnya mereka menganggap semua kejadian mistis hanyalah bumbu konten, namun perlahan suasana berubah mencekam ketika satu per satu mengalami kejadian supranatural yang mengancam nyawa mereka. Film ini memadukan unsur horor yang menegangkan dengan humor segar khas anak muda, sambil menyisipkan kritik sosial tentang obsesi terhadap viralitas di era digital.",
     "actors": [
       {"name": "Omara Esteghlal", "photo": "assets/images/actors/omara.jpg"},
       {"name": "Mawar de Jongh", "photo": "assets/images/actors/mawar.jpg"},
@@ -135,6 +153,8 @@ List<Map<String, dynamic>> recommendations = [
     "genre": "Comedy",
     "duration": "1hr 59min",
     "status": "XXI,NETFLIX,Cinepolis ",
+    "synopsis":
+        " Tempat sahabat—Bene, Boris, Oki, dan Indra—yang bekerja di sebuah rumah hantu di pasar malam. Dalam usaha menaikkan popularitas wahana mereka, keempatnya melakukan modifikasi ekstrem agar lebih menyeramkan. Namun, semuanya berubah kacau saat seorang pengunjung benar-benar tewas di dalam rumah hantu akibat kesalahan mereka. Panik dan takut dipenjara, mereka mencoba menutupi kejadian itu, tapi justru berujung pada serangkaian peristiwa ganjil dan supranatural. Film ini menggabungkan humor absurd dengan nuansa horor dan kritik sosial, menghadirkan kisah yang segar, kocak, dan menegangkan dalam satu paket hiburan yang unik.",
     "actors": [
       {"name": "Indra Jegel", "photo": "assets/images/actors/jegel.jpg"},
       {"name": "Boris Bokir", "photo": "assets/images/actors/boris.jpg"},
@@ -152,6 +172,8 @@ List<Map<String, dynamic>> recommendations = [
     "genre": "Drama, Romance",
     "duration": "1hr 43min",
     "status": "XXI, Cinepolis, CGV",
+    "synopsis":
+        "kehidupan anak-anak jalanan yang tumbuh besar di bawah kolong jembatan, jauh dari kasih sayang keluarga dan fasilitas layak. Fokus cerita terpusat pada seorang anak bernama Rehan yang hidup bersama kelompok kecil anak-anak senasib dan mencoba bertahan hidup di tengah kerasnya kota. Meski dikelilingi kemiskinan, kekerasan, dan godaan kejahatan, Rehan tetap memegang teguh prinsipnya untuk tidak terjerumus ke hal-hal negatif. Film ini menyajikan potret nyata perjuangan anak-anak marginal, menggugah empati, dan menjadi kritik terhadap ketimpangan sosial serta kurangnya perhatian negara terhadap masa depan generasi muda di jalanan.",
     "actors": [
       {"name": "Junior Roberts", "photo": "assets/images/actors/junior.jpg"},
       {"name": "Aisyah Aqilah", "photo": "assets/images/actors/aisyah.jpg"},
@@ -169,6 +191,8 @@ List<Map<String, dynamic>> recommendations = [
     "genre": "Drama, Romance",
     "duration": "1hr 37min",
     "status": "Amazon Prime Video, Prime",
+    "synopsis":
+        "pertemuan tak terduga antara seorang terapis emosional bernama Tania dan pasiennya, Ares, seorang pria muda yang mengalami trauma cinta akibat masa lalunya yang kelam. Dalam setiap sesi terapi, hubungan mereka perlahan berkembang dari profesional menjadi personal, hingga keduanya terjebak dalam dilema perasaan yang tak seharusnya tumbuh di antara terapis dan klien. Di tengah konflik batin dan pertanyaan etika, mereka mencoba mencari arti cinta yang sebenarnya—apakah cinta bisa menyembuhkan, atau justru menjadi luka baru? Film ini menyuguhkan perjalanan emosional yang dalam, dengan nuansa psikologis dan romantisme yang kuat.",
     "actors": [
       {"name": "Fachry Albar ", "photo": "assets/images/actors/albar.jpg"},
       {"name": "Wulan Guritno", "photo": "assets/images/actors/wulan.jpg"},
@@ -186,6 +210,8 @@ List<Map<String, dynamic>> recommendations = [
     "genre": "Action, Horror, Comedy",
     "duration": "1hr 52min",
     "status": "XXI, Cinepolis, NETFLIX",
+    "synopsis":
+        "petualangan remaja Indonesia yang bercerita tentang lima sahabat—Raka, Bimo, Naya, Tika, dan Jalu—yang tanpa sengaja terlibat dalam sebuah misteri kuno saat menjelajahi hutan untuk tugas sekolah. Petualangan mereka berubah menjadi pencarian berbahaya ketika mereka menemukan petunjuk menuju harta peninggalan leluhur yang telah lama tersembunyi. Dalam perjalanan, mereka menghadapi berbagai rintangan, termasuk konflik antar teman, jebakan alam, dan kejaran pihak yang ingin merebut harta tersebut. Namun, semangat kebersamaan, keberanian, dan kepercayaan satu sama lain menjadi kunci utama mereka untuk keluar dari situasi berbahaya. Film ini memadukan unsur petualangan, persahabatan, dan nilai-nilai moral yang menginspirasi penonton muda.",
     "actors": [
       {"name": "Bayu Skak", "photo": "assets/images/actors/skak.jpg"},
       {"name": "Keisya Levronka", "photo": "assets/images/actors/keisya.jpg"},
